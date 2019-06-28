aa853f9db4e70123327559d4ada4a776974bfc01
Emil Nordling <emil.nordling@visibacare.com>
2019-06-28 10:04:18 +0200

    Disable react specific modules if react is not present

--- src/babel/babel.config.js
+++ src/babel/babel.config.js
@@ -2,57 +2,67 @@ const getConfig = () => {
   const { getPaths } = require('../paths');
   const paths = getPaths();
 
-  return {
-    presets: [
-      ['@babel/preset-env', {
-        'modules': false,
-        'targets': {
-          'chrome': 70,
-          'firefox': 64,
-        },
-      }],
-      '@babel/react',
-      '@babel/typescript',
-    ],
+  const presetsRunTime = [
+    ['@babel/preset-env', {
+      'modules': false,
+      'targets': {
+        'chrome': 70,
+        'firefox': 64,
+      },
+    }],
+    '@babel/typescript',
+  ];
+
+  const presetsTest = [
+    ['@babel/preset-env', {
+      'modules': 'commonjs',
+      'targets': {
+        'chrome': 70,
+        'firefox': 64,
+      },
+    }],
+    '@babel/typescript',
+  ];
+
+  const config = {
+    presets: presetsRunTime,
     plugins: [
       'preval',
-      '@babel/plugin-transform-typescript',
-      ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-      'react-hot-loader/babel',
+      '@babel/transform-typescript',
+      ['@babel/proposal-decorators', { 'legacy': true }],
       ['module-resolver', {
         'extensions': ['.js', '.jsx', '.ts', '.tsx'],
         'root': [paths.appSrc || './src']
       }],
-      '@babel/plugin-syntax-dynamic-import',
-      ['@babel/plugin-proposal-class-properties', { 'loose': true }],
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-proposal-numeric-separator',
-      '@babel/plugin-proposal-throw-expressions',
-      '@babel/plugin-syntax-import-meta',
-      '@babel/plugin-proposal-json-strings',
-      '@babel/plugin-proposal-optional-catch-binding',
+      '@babel/syntax-dynamic-import',
+      ['@babel/proposal-class-properties', { 'loose': true }],
+      '@babel/proposal-object-rest-spread',
+      '@babel/transform-runtime',
+      '@babel/proposal-function-sent',
+      '@babel/proposal-export-namespace-from',
+      '@babel/proposal-numeric-separator',
+      '@babel/proposal-throw-expressions',
+      '@babel/syntax-import-meta',
+      '@babel/proposal-json-strings',
+      '@babel/proposal-optional-catch-binding',
       'styled-components',
     ],
     "env": {
       "test": {
-        presets: [
-          ['@babel/preset-env', {
-            'modules': 'commonjs',
-            'targets': {
-              'chrome': 70,
-              'firefox': 64,
-            },
-          }],
-          '@babel/react',
-          '@babel/typescript',
-        ],
+        presets: presetsTest,
       },
     },
   };
-}
 
+  try {
+    require.resolve('react');
+
+    presetsRunTime.splice(1, 0, '@babel/react');
+    presetsTest.splice(1, 0, '@babel/react');
+    config.plugins.splice(3, 0, 'react-hot-loader/babel');
+  } finally {
+    return config;
+  }
+}
 
 module.exports = getConfig;
